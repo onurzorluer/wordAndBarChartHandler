b61e2c98258ff1299a9c6572c2fbead7124a7c98
Onur Zorluer <onurzorluer1@gmail.com>
2017-10-09 03:13:25 +0300

    Add files via upload

--- src/project3/WordHandler.java
+++ src/project3/WordHandler.java
@@ -1,4 +1,3 @@
-package project3;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
@@ -21,18 +20,34 @@ import org.openxmlformats.schemas.wordprocessingml.x2006.main.STFldCharType;
 public class WordHandler {
 
 	private XWPFDocument document = null;
-
+	
 	public void addParagraph(String paragraphContent, XWPFDocument doc) {
 		// create Paragraph
 		XWPFParagraph paragraphOne = doc.createParagraph();
-		XWPFRun paragraphOneRun = paragraphOne.createRun();
-		paragraphOneRun.setFontSize(12);
 		paragraphOne.setBorderLeft(Borders.DOUBLE);
 		paragraphOne.setBorderRight(Borders.DOUBLE);
+		XWPFRun paragraphOneRun = paragraphOne.createRun();
+		paragraphOneRun.setFontSize(10);
+
 		paragraphOneRun.setText(paragraphContent);
 		paragraphOneRun.addBreak();
 
 	}
+	
+	public void addBoldParagraph(String paragraphContent, XWPFDocument doc) {
+		// create Paragraph
+		XWPFParagraph paragraphTwo = doc.createParagraph();
+		paragraphTwo.setBorderLeft(Borders.DOUBLE);
+		paragraphTwo.setBorderRight(Borders.DOUBLE);
+		XWPFRun paragraphTwoRun = paragraphTwo.createRun();
+		paragraphTwoRun.setBold(true);
+		paragraphTwoRun.setFontSize(10);
+
+		paragraphTwoRun.setText(paragraphContent);
+		paragraphTwoRun.addBreak();
+
+	}
+
 
 	public void addHeader(String headerContent, XWPFDocument doc) {
 		// Create Header
@@ -42,14 +57,41 @@ public class WordHandler {
 		header.setBorderRight(Borders.DOUBLE);
 		XWPFRun headerRun = header.createRun();
 		headerRun.setBold(true);
-		headerRun.setFontSize(24);
+		headerRun.setFontSize(18);
 
 		headerRun.setText(headerContent);
 		headerRun.addBreak();
 	}
+	
+	public void addSecondHeader(String secondHeaderContent, XWPFDocument doc) {
+		// Create Second Header
+		XWPFParagraph secondHeader = doc.createParagraph();
+		XWPFRun headerRun = secondHeader.createRun();
+		secondHeader.setAlignment(ParagraphAlignment.CENTER);
+		secondHeader.setBorderLeft(Borders.DOUBLE);
+		secondHeader.setBorderRight(Borders.DOUBLE);
+		headerRun.setBold(true);
+		headerRun.setFontSize(14);
+
+		headerRun.setText(secondHeaderContent);
+		headerRun.addBreak();
+	}
+	
+	public void addThirdHeader(String thirdHeaderContent, XWPFDocument doc) {
+		// Create Second Header
+		XWPFParagraph thirdHeader = doc.createParagraph();
+		thirdHeader.setBorderLeft(Borders.DOUBLE);
+		thirdHeader.setBorderRight(Borders.DOUBLE);
+		XWPFRun headerRun = thirdHeader.createRun();
+		headerRun.setBold(true);
+		headerRun.setFontSize(12);
+
+		headerRun.setText(thirdHeaderContent);
+		headerRun.addBreak();
+	}
 
 	public void addDoubleLine(XWPFDocument doc) {
-		// create Line
+		// create Double Line
 		XWPFParagraph line = doc.createParagraph();
 		line.setBorderBottom(Borders.DOUBLE);
 		line.setBorderLeft(Borders.DOUBLE);
@@ -60,9 +102,19 @@ public class WordHandler {
 	}
 
 	public void addSingleLine(XWPFDocument doc) {
-		// create Line
+		// create Single Line
 		XWPFParagraph line = doc.createParagraph();
 		line.setBorderBottom(Borders.SINGLE);
+		line.setBorderLeft(Borders.DOUBLE);
+		line.setBorderRight(Borders.DOUBLE);
+		XWPFRun lineRun = line.createRun();
+		lineRun.setText(" ");
+
+	}
+	
+	public void addBlankLine(XWPFDocument doc) {
+		// create Blank Line
+		XWPFParagraph line = doc.createParagraph();
 		line.setBorderLeft(Borders.DOUBLE);
 		line.setBorderRight(Borders.DOUBLE);
 		XWPFRun lineRun = line.createRun();
@@ -85,9 +137,12 @@ public class WordHandler {
 		pictureOne.setAlignment(ParagraphAlignment.CENTER);
 		pictureOne.setBorderLeft(Borders.DOUBLE);
 		pictureOne.setBorderRight(Borders.DOUBLE);
+		pictureOne.setBorderTop(Borders.DOUBLE);
+		pictureOne.setBorderBottom(Borders.DOUBLE);
 		try {
-			pictureOneRun.addPicture(is, XWPFDocument.PICTURE_TYPE_PNG, imgFilePath, Units.toEMU(300),
-					Units.toEMU(200));
+			pictureOneRun.addPicture(is, XWPFDocument.PICTURE_TYPE_PNG, imgFilePath, Units.toEMU(420),
+					Units.toEMU(280));
+			
 		} catch (InvalidFormatException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -104,14 +159,6 @@ public class WordHandler {
 
 	}
 	
-	public XWPFDocument getDocument() {
-		return document;
-	}
-
-	public void setDocument(XWPFDocument document) {
-		this.document = document;
-	}
-
 	public void addRightandLeftLogo(String imgFilePathOne, String imgFilePathTwo, XWPFDocument doc)
 	{
 		XWPFParagraph pictureTwo = doc.createParagraph();
@@ -128,14 +175,15 @@ public class WordHandler {
 		}
 		pictureTwoRun.addBreak();
 		pictureTwo.setAlignment(ParagraphAlignment.CENTER);
+		pictureTwo.setBorderTop(Borders.DOUBLE);
 		pictureTwo.setBorderLeft(Borders.DOUBLE);
 		pictureTwo.setBorderRight(Borders.DOUBLE);
 		try {
 			pictureTwoRun.addPicture(is1, XWPFDocument.PICTURE_TYPE_JPEG, imgFilePathOne, Units.toEMU(120),
-					Units.toEMU(80));
-			pictureTwoRun.setText("                                                        ");
+					Units.toEMU(120));
+			pictureTwoRun.setText("                                                          ");
 			pictureTwoRun.addPicture(is2, XWPFDocument.PICTURE_TYPE_JPEG, imgFilePathTwo, Units.toEMU(120),
-					Units.toEMU(80));
+					Units.toEMU(120));
 		} catch (InvalidFormatException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -151,8 +199,7 @@ public class WordHandler {
 			e.printStackTrace();
 		}
 	}
-	
-	public void setFooter(XWPFDocument document) {
+	public void addPageNumbers(XWPFDocument document) {
 	    CTP ctpFooter = CTP.Factory.newInstance();
 	    ctpFooter.addNewR().addNewT();
 
@@ -176,5 +223,13 @@ public class WordHandler {
 	    CTSectPr sectPr = document.getDocument().getBody().addNewSectPr();
 	    XWPFHeaderFooterPolicy policy = new XWPFHeaderFooterPolicy(document, sectPr);
 	    policy.createFooter(XWPFHeaderFooterPolicy.DEFAULT, parsFooter);
+	}
+	
+	public XWPFDocument getDocument() {
+		return document;
+	}
+
+	public void setDocument(XWPFDocument document) {
+		this.document = document;
 	}
 }
